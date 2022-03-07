48070b58ce34d0ba1853d10642d6374ac272c0d4
jay2554 <jay@mybixie.com>
2022-03-07 12:23:56 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
           <div className="col-md-5">
             <SignUp />
           </div>
-          <div className="col-md-7">
+          <div className="col-md-7 my-auto">
             <img className="img-fluid w-100" src="login.jpg" alt = "login" /> 
           </div>
         </div>
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,16 +1,38 @@
 import React from 'react'
 import { Formik, Form } from 'formik'
 import {TextField} from './TextField'
+import * as Yup from 'yup'
 
 const SignUp = () => {
+    const validate = Yup.object({
+        firstName: Yup.string()
+                    .max(20, 'Maximum length can be 20')
+                    .required("FirstName is required"),
+        lastName: Yup.string()
+                    .max(20, 'Maximum length can be 20')
+                    .required("LastName is required"),
+        email: Yup.string()
+                    .email('Please Enter valid Email')
+                    .required("Email is required"),
+        password: Yup.string()
+                    .min(6, 'Atleast 6 characters required')
+                    .required("passsword is required"),
+        confirmPassword: Yup.string() 
+                    .oneOf([Yup.ref('password'),null], 'Password does not match')
+                    .required("Confirm Password is required"),
+    })
   return (
     <Formik
         initialValues={{
             firstName : '',
-            LastName: '', 
+            lastName: '', 
             email: '',
             password: '',
             confirmPassword: ''
+        }}
+        validationSchema={validate}
+        onSubmit={values => {
+            console.log(values);
         }}
     >
         {
@@ -18,7 +40,13 @@ const SignUp = () => {
                 <div>
                     <h1 className="h1 my-4 font-weight-bold-display-4">Sign Up</h1>
                     <Form>
-                       <TextField label="First Name" />
+                       <TextField label="First Name" name = "firstName" type="text" />
+                       <TextField label="Last Name" name = "lastName" type="text" />
+                       <TextField label="Email" name = "email" type="email" />
+                       <TextField label="Password" name = "password" type="password" />
+                       <TextField label="Confirm Password" name = "confirmPassword" type="password" />
+                       <button className="btn btn-dark mt-3" type="submit">Register</button>
+                       <button className="btn btn-danger mt-3 ml-3" style = {{marginLeft:'3px'}} type="reset">Reset</button>
                     </Form>
                 </div> 
             )
